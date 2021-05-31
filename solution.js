5945b234adb7f9268d5931ee381f272545e50466
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-05-31 18:03:07 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,8 @@
+* Solution 1: Modifying JavaScript
+
+<script>
+
+document.getElementsByTagName("h2")[2].innerHTML = "Modified You";
+document.getElementsByTagName("h1")[0].innerHTML = "Found You";
+
+</script>
