0e7908c195665bf305e02d73e66f5df50568eb66
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-05-31 18:17:56 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,8 +1,24 @@
-* Solution 1: Modifying JavaScript
+* Solution 1: Modifying HTML
 
 <script>
 
 document.getElementsByTagName("h2")[2].innerHTML = "Modified You";
 document.getElementsByTagName("h1")[0].innerHTML = "Found You";
+
+</script>
+
+
+* Solution 2: Changing Links
+
+<script>
+
+var links = document.getElementsByTagName("a");
+
+for  (i=0; i < links.length; i++)
+{
+  links[i].href = "http://PentesterAcademy.com/topics";
+  links[i].innerHTML = "Link Modified";
+  
+}
 
 </script>
