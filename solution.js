6a8f69968c7299f17d54afc5121891b1568bfd75
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2021-06-02 20:33:30 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -198,7 +198,9 @@ req.send();
 var req = new XMLHttpRequest();
 req.onreadystatechange = function ()
 
-if (req.readyState == 4 && req.status ==200)
+{
+
+	if (req.readyState == 4 && req.status ==200)
 	
 {
 	alert(req.responseText);
